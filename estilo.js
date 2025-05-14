53afa77b842d91812e0af1fe20f50ec0f45fb132
TOBAR MINA DAVID ALEJANDRO <datobarmi@pucesi.edu.ec>
2025-05-14 17:23:53 -0500

    Primer commit

---
+++ estilo.js
@@ -0,0 +1,7 @@
+function verProductos() {
+  document.getElementById('productos').scrollIntoView({ behavior: 'smooth' });
+}
+
+function agregarCarrito(producto) {
+  alert(`Has agregado "${producto}" al carrito.`);
+}
